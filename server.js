b114d480d2a300af237fd8d639705afc1fdeaa72
Clarisagonzalez <136874433+Clarisagonzalez@users.noreply.github.com>
2023-11-29 02:21:34 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+const express = require("express");
+const fs = require("fs");
+const path = require("path"); // Add path module for file operations
+
+// Sets up the express app
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/public/assets", express.static(__dirname + "/public/assets"));
+
+// Require routes
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
